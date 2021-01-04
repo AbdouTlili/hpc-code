d76d78e49a77a89efab72cef17097417dca53e6f
zacnnnt <atlili@brenco-algerie.com>
2021-01-04 23:58:33 +0100

    2nd example added to Pthread

---
+++ pthread/simple_print_thread2.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#define NUM_THREADS 5
+
+void* printHello(void *threadid){
+    long tid ;
+    tid = (long) threadid;
+    printf("Hello it's me, thread %ld ! \n",tid);
+    pthread_exit(NULL);
+}
+
+int main (char argc, char *argv[]){
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+    for(t=0;t<NUM_THREADS;t++){
+        printf("in main : creating thread %ld \n",t);
+        rc = pthread_create(&threads[t],NULL,printHello,(void*)t);
+        if(rc){
+            printf("ERROR ; return code from pthread_create() is %d\n",rc);
+        }
+
+    }
+}
