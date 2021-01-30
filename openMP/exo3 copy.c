a96c866a499415da02c4bb6e6b2eb355b7b4f596
zacnnnt <ha_tlili@esi.dz>
2021-01-30 16:34:45 +0100

    equation system resolver with openMP added

--- openMP/exo3 copy.c
+++ openMP/exo3 copy.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main (){
-static long nb_pas = 100000;
-double pas;
-int i; double x, pi, som = 0.0;
-pas = 1.0/(double) nb_pas;
-for (i=1; i<= nb_pas; i++){
-x = (i-0.5)*pas;
-som = som + 4.0/(1.0+x*x);
-}
-pi = pas * som;
-printf("PI=%f\n",pi);
-return 0;
+    static long nb_pas = 100000;
+    double pas;
+    int i; double x, pi, som = 0.0;
+    pas = 1.0/(double) nb_pas;
+    for (i=1; i<= nb_pas; i++){
+        x = (i-0.5)*pas;
+        som = som + 4.0/(1.0+x*x);
+    }
+    pi = pas * som;
+    printf("PI=%f\n",pi);
+    return 0;
 }
